46a6291a81fa8f531849bec636bf1a7fe679b2d3
Steffen Butzer <steffen.butzer@outlook.com>
2017-10-21 16:37:28 +0200

    server: do not send error content to client, bind test mode to environment

--- server/app.ts
+++ server/app.ts
@@ -6,6 +6,7 @@ import {SongController} from "./controllers/SongController";
 
 // creates express app, registers all controller routes and returns you express app instance
 const app = createExpressServer({
+    defaultErrorHandler: false,
     controllers: [UserController, SongController] // we specify controllers we want to use
 });
 
